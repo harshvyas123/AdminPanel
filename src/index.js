2211de350ff8f98fcfa9dedce4b8512244e0354c
harsh <harshv.bba2021@ssism.org>
2023-07-29 12:56:08 +0530

    admin panel

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignInSignupWithLocalStorage from './SignInSingUp/SignInSingUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SignInSignupWithLocalStorage/>
   </React.StrictMode>
 );
 
